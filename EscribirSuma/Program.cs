2474575d85a46738fb366b33d07b2929a9161258
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-07 00:12:04 -0300

    EscriibirSuma

---
+++ EscribirSuma/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EscribirSuma
+{
+    class Program
+    {
+        public static void EscribirSuma(int a, int b)
+        {
+            Console.WriteLine(a+b);
+        }
+        public static void Main()
+        {
+            Console.Write("La suma de 4 y 7 es: ");
+            EscribirSuma(4,7);
+
+            Console.ReadKey();
+        }
+
+    }
+}
